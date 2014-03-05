c877ff385d46976e98f1c8544bb0428a5df5654e
Jonas Hess <jonas.hess@revier.de>
2014-03-05 17:55:34 +0100

    domain!

--- copy_this/modules/new_customer_notification.php
+++ copy_this/modules/new_customer_notification.php
@@ -7,7 +7,7 @@ class new_customer_notification extends new_customer_notification_parent
        if ($parent_return == true)
        {
            $oEmail = oxNew( 'oxemail' );
-		   $my_mailtext = "Hallo,\n\ngerade eben hat sich ein neuer User im OnlineShop www.jaeckle-sst.de/shop registriert.\n";
+		   $my_mailtext = "Hallo,\n\ngerade eben hat sich ein neuer User im OnlineShop www.kundenshop.de/shop registriert.\n";
 		   $my_mailtext .= "\n\nBenutzername: " . $this->oxuser__oxusername->value;
 		   $my_mailtext .= "\n\nHerzliche Gruesse\nvom Shopsystem ;-)";
            $oEmail->sendEmail('nnn@webshop.nn', 'Neukunden Anmeldung',  $my_mailtext);
